9f1cbce090e1322bccfeeb628309946c1f64015b
potakaaa <helbirog@gmail.com>
2025-01-02 18:11:23 +0800

    delete(navbar): cant be spread evenly

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <div className="size-full items-center justify-center flex flex-col">
-        <Navbar />
         App
       </div>
     </ThemeProvider>
