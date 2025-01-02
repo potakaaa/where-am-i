22e0cb6a9844556e42f8ae5d7705b300bcfbc765
potakaaa <helbirog@gmail.com>
2025-01-02 20:50:57 +0800

    edit(navbar): added loading state for skeletons on tech stack icons

--- src/components/navbar/TechStack.tsx
+++ src/components/navbar/TechStack.tsx
@@ -7,8 +7,11 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useState } from "react";
 
 export function TechStack() {
+  const [isLoaded, setIsLoaded] = useState(true);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -20,30 +23,40 @@ export function TechStack() {
         <div className="flex flex-col">
           <div className="flex self-stretch justify-evenly">
             <a href="https://react.dev/">
-              <Avatar className="size-8 shadow">
-                <AvatarImage src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+              <Avatar className="size-8">
+                <AvatarImage
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+                  onLoad={() => setIsLoaded(false)}
+                />
                 <AvatarFallback>React</AvatarFallback>
               </Avatar>
             </a>
             <a href="https://vite.dev/">
-              <Avatar className="size-8 shadow">
-                <AvatarImage src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" />
+              <Avatar className="size-8">
+                <AvatarImage
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+                  onLoad={() => setIsLoaded(false)}
+                />
                 <AvatarFallback>Vite</AvatarFallback>
               </Avatar>
             </a>
             <a href="https://www.typescriptlang.org/">
-              <Avatar className="size-8 shadow">
-                <AvatarImage src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+              <Avatar className="size-8">
+                <AvatarImage
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+                  onLoad={() => setIsLoaded(false)}
+                />
                 <AvatarFallback>Typescript</AvatarFallback>
               </Avatar>
             </a>
             <a href="https://tailwindcss.com/">
-              <a href="https://ui.shadcn.com/">
-                <Avatar className="size-8 shadow">
-                  <AvatarImage src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
-                  <AvatarFallback>Tailwind</AvatarFallback>
-                </Avatar>
-              </a>
+              <Avatar className="size-8">
+                <AvatarImage
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+                  onLoad={() => setIsLoaded(false)}
+                />
+                <AvatarFallback>Tailwind</AvatarFallback>
+              </Avatar>
             </a>
           </div>
           <div className="flex pt-4 items-center justify-center">
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
+  base: "/where-am-i/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
