509afd08586ac2a5e9c4c86251c790db0ff29730
potakaaa <helbirog@gmail.com>
2025-01-03 16:10:16 +0800

    edit(navbar): added skeleton for loading icons on tech stack

--- src/components/navbar/HowToUse.tsx
+++ src/components/navbar/HowToUse.tsx
@@ -21,21 +21,15 @@ export function HowToUse() {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-72 p-4 m-2">
         <div className="flex justify-between space-x-4">
-          <Avatar>
-            <AvatarImage src="https://github.com/vercel.png" />
+          <Avatar className="size-8">
+            <AvatarImage src="public\logo.svg" />
             <AvatarFallback>VC</AvatarFallback>
           </Avatar>
           <div className="space-y-1">
-            <h4 className="text-sm font-semibold">@nextjs</h4>
             <p className="text-sm">
-              The React Framework – created and maintained by @vercel.
+              Click the <strong>Where am I?</strong> button to see your current
+              location.
             </p>
-            <div className="flex items-center pt-2">
-              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
-              <span className="text-xs text-muted-foreground">
-                Joined December 2021
-              </span>
-            </div>
           </div>
         </div>
       </DropdownMenuContent>
